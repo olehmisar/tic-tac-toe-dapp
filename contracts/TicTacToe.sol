0e48b2d8e59e03fd533ca9d60679033af1a3fcd0
Oleh Misarosh <olehmisar@gmail.com>
2021-08-13 21:22:36 +0300

    Add TicTacToe

---
+++ contracts/TicTacToe.sol
@@ -0,0 +1,166 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.4;
+
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+
+struct Game {
+    address player0;
+    address player1;
+    address lastPlayer;
+    address[][] board;
+    address winner;
+}
+
+struct Move {
+    address player;
+    uint256 i;
+    uint256 j;
+}
+
+struct Proof {
+    uint256 gameId;
+    address winner;
+}
+
+contract TicTacToe {
+    error BadSignature(address signer);
+
+    Game[] public getGame;
+
+    function gamesLength() external view returns (uint256) {
+        return getGame.length;
+    }
+
+    function startGame(address player0, address player1) external {
+        getGame.push(Game({
+            player0: player0,
+            player1: player1,
+            lastPlayer: player1,  // player0 should start
+            board: _emptyBoard(),
+            winner: address(0)
+        }));
+    }
+
+    function endGameWithMoves(
+        uint256 gameId,
+        Move[] calldata moves,
+        bytes calldata mySig,
+        bytes calldata opponentSig
+    ) external {
+        Game storage game = getGame[gameId];
+        require(game.winner == address(0), "game ended");
+        (address me, address opponent) = _validateMsgSender(game.player0, game.player1);
+        _verifyMoves(gameId, moves, me, mySig);
+        _verifyMoves(gameId, moves[0:moves.length - 1], opponent, opponentSig);
+
+        // Copy to memory to be able to modify it using pure functions.
+        Game memory state = game;
+        for (uint256 i = 0; i < moves.length; i++) {
+            doMove(state, moves[i]);
+        }
+        require(checkWinner(state.board, me), "!winner");
+        game.winner = me;
+    }
+
+    function encodeMoves(uint256 gameId, Move[] calldata moves) public pure returns (bytes32) {
+        return keccak256(abi.encode(gameId, moves));
+    }
+
+    function _verifyMoves(
+        uint256 gameId,
+        Move[] calldata moves,
+        address signer,
+        bytes calldata signature
+    ) private pure {
+        if (_recover(encodeMoves(gameId, moves), signature) != signer) {
+            revert BadSignature(signer);
+        }
+    }
+
+    function _validateMsgSender(address player0, address player1) private view returns (address me, address opponent) {
+        if (player0 == msg.sender) {
+            (me, opponent) = (player0, player1);
+        } else {
+            require(player1 == msg.sender, "!player");
+            (me, opponent) = (player1, player0);
+        }
+    }
+
+    function _recover(bytes32 hash, bytes calldata signature) private pure returns (address) {
+        return ECDSA.recover(ECDSA.toEthSignedMessageHash(hash), signature);
+    }
+
+    // GAME LOGIC
+    function doMove(Game memory state, Move calldata move) public pure returns (Game memory) {
+        require(state.board[move.i][move.j] == address(0), "!empty");
+        require(move.player != state.lastPlayer, "!turn");
+        state.board[move.i][move.j] = move.player;
+        state.lastPlayer = move.player;
+        return state;
+    }
+
+    function checkWinner(address[][] memory board, address winner) public pure returns (bool) {
+        (uint256 rows, uint256 cols) = (board.length, board[0].length);
+        require(rows == cols, "rows != cols");
+
+        // Check verticals
+        for (uint256 i = 0; i < rows; i++) {
+            uint256 sum = 0;
+            for (uint256 j = 0; j < cols; j++) {
+                if (board[i][j] == winner) {
+                    sum += 1;
+                }
+            }
+            if (sum == cols) {
+                return true;
+            }
+        }
+
+        // Check horizontals
+        for (uint256 j = 0; j < cols; j++) {
+            uint256 sum = 0;
+            for (uint256 i = 0; i < rows; i++) {
+                if (board[i][j] == winner) {
+                    sum += 1;
+                }
+            }
+            if (sum == rows) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        {
+            uint256 sum = 0;
+            for (uint256 i = 0; i < rows; i++) {
+                if (board[i][i] == winner) {
+                    sum += 1;
+                }
+            }
+            if (sum == rows) {
+                return true;
+            }
+        }
+        {
+            uint256 sum = 0;
+            for (uint256 i = rows; i > 0; i--) {
+                if (board[i-1][i-1] == winner) {
+                    sum += 1;
+                }
+            }
+            if (sum == rows) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function _emptyBoard() private pure returns (address[][] memory board) {
+        board = new address[][](3);
+        board[0] = new address[](3);
+        board[1] = new address[](3);
+        board[2] = new address[](3);
+    }
+}
---
+++ test/TicTacToe.spec.ts
@@ -0,0 +1,115 @@
+import { arrayify } from '@ethersproject/bytes';
+import { AddressZero } from '@ethersproject/constants';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { TicTacToe } from '../typechain';
+
+interface Move {
+  player: string;
+  i: number;
+  j: number;
+}
+
+describe('TicTacToe', () => {
+  let player0Account: SignerWithAddress, player1Account: SignerWithAddress;
+  let player0: string, player1: string;
+  let lobby: TicTacToe;
+  beforeEach(async () => {
+    [player0Account, player1Account] = await ethers.getSigners();
+    [player0, player1] = [player0Account, player1Account].map((acc) => acc.address);
+
+    lobby = await (await ethers.getContractFactory('TicTacToe')).deploy();
+  });
+
+  describe('#startGame', () => {
+    it('should create a game object', async () => {
+      await lobby.startGame(player0, player1);
+      const game = await lobby.getGame(0);
+      expect(game.player0).to.eq(player0);
+      expect(game.player1).to.eq(player1);
+      expect(game.winner).to.eq(AddressZero);
+    });
+  });
+
+  describe('#endGameWithMoves', () => {
+    let gameId: number;
+    let validMoves: Move[];
+    beforeEach(async () => {
+      await lobby.startGame(player0, player1);
+      expect(await lobby.gamesLength()).to.eq(1); // sanity check
+      gameId = 0;
+      validMoves = [
+        { player: player0, i: 0, j: 0 },
+        { player: player1, i: 1, j: 0 },
+        { player: player0, i: 0, j: 1 },
+        { player: player1, i: 1, j: 1 },
+        { player: player0, i: 0, j: 2 },
+      ];
+    });
+
+    async function signMoves(signer: SignerWithAddress, gameId: number, moves: Move[]) {
+      return await signer.signMessage(arrayify(await lobby.encodeMoves(gameId, moves)));
+    }
+
+    async function signMovesForBoth(gameId: number, moves: Move[]) {
+      return await Promise.all([
+        signMoves(player0Account, gameId, moves),
+        signMoves(player1Account, gameId, moves.slice(0, moves.length - 1)),
+      ]);
+    }
+
+    it('should end a game with valid moves', async () => {
+      const sig0 = await signMoves(player0Account, gameId, validMoves);
+      const sig1 = await signMoves(player1Account, gameId, validMoves.slice(0, validMoves.length - 1));
+      await lobby.endGameWithMoves(gameId, validMoves, sig0, sig1);
+      expect((await lobby.getGame(gameId)).winner).to.eq(player0);
+    });
+
+    it('should NOT end the game with invalid winner signature', async () => {
+      const sig0 = await signMoves(player0Account, gameId, validMoves.slice(0, 1));
+      const sig1 = await signMoves(player1Account, gameId, validMoves.slice(0, validMoves.length - 1));
+      await expect(lobby.endGameWithMoves(gameId, validMoves, sig0, sig1)).to.be.revertedWith(
+        `custom error 'BadSignature("${player0}")'`,
+      );
+    });
+
+    it('should NOT end the game with invalid loser signature', async () => {
+      const sig0 = await signMoves(player0Account, gameId, validMoves);
+      const sig1 = await signMoves(player1Account, gameId, validMoves);
+      await expect(lobby.endGameWithMoves(gameId, validMoves, sig0, sig1)).to.be.revertedWith(
+        `custom error 'BadSignature("${player1}")'`,
+      );
+    });
+
+    it('should NOT allow player1 to start', async () => {
+      const moves = [{ player: player1, i: 0, j: 0 }];
+      const [sig0, sig1] = await signMovesForBoth(gameId, moves);
+      await expect(lobby.endGameWithMoves(gameId, moves, sig0, sig1)).to.be.revertedWith('!turn');
+    });
+
+    it('should NOT allow two consecutive moves', async () => {
+      const moves = [
+        { player: player0, i: 0, j: 0 },
+        { player: player0, i: 0, j: 1 },
+      ];
+      const [sig0, sig1] = await signMovesForBoth(gameId, moves);
+      await expect(lobby.endGameWithMoves(gameId, moves, sig0, sig1)).to.be.revertedWith('!turn');
+    });
+
+    it('should NOT allow to check already checked cell', async () => {
+      const moves = [
+        { player: player0, i: 0, j: 0 },
+        { player: player1, i: 0, j: 0 },
+      ];
+      const [sig0, sig1] = await signMovesForBoth(gameId, moves);
+      await expect(lobby.endGameWithMoves(gameId, moves, sig0, sig1)).to.be.revertedWith('!empty');
+    });
+
+    it('should NOT allow to request game end if game is already ended', async () => {
+      const [sig0, sig1] = await signMovesForBoth(gameId, validMoves);
+      await lobby.endGameWithMoves(gameId, validMoves, sig0, sig1);
+      await expect(lobby.endGameWithMoves(gameId, validMoves, sig0, sig1)).to.be.revertedWith('game ended');
+    });
+  });
+});
